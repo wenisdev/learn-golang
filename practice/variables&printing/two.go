67f9ce9f095c9c6c36c0a58888e583733b611b3d
waynetabalanza <tabalanzawayne@gmail.com>
2026-05-21 23:35:35 +0800

    two

--- practice/variables&printing/two.go
+++ practice/variables&printing/two.go
@@ -6,13 +6,8 @@ func two() {
 
 	// Store your name and age then print them
 
-	fmt.Print("Enter your First Name: ")
-	var firstname string
-	fmt.Scanln(&firstname)
-
-	fmt.Print("Enter your Last Name: ")
-	var lastname string
-	fmt.Scanln(&lastname)
+	var firstname string = "John"
+	var lastname string = "Doe"
 
 	fmt.Printf("Your full name is: %s %s\n", firstname, lastname)
 
