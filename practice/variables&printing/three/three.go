03d886c6b68306771f168e294562968898dd4159
waynetabalanza <tabalanzawayne@gmail.com>
2026-05-22 10:18:48 +0800

    three: Create variables for width and height then calculate area

---
+++ practice/variables&printing/three/three.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Create variables for width and height then calculate area
+
+	var width float64 = 4.3
+	var height float64 = 5.2
+	area := width * height
+	fmt.Println("Area of the rectangle is:", area)
+
+}
