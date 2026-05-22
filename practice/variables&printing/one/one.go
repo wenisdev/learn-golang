fccfec98b23a97aab8972cc7c4b50777d1eb150f
waynetabalanza <tabalanzawayne@gmail.com>
2026-05-22 10:17:08 +0800

    one: use variables x and y to print their sum

---
+++ practice/variables&printing/one/one.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Create 2 variables x and y then print their sum
+
+	var x int = 10
+	var y int = 20
+	fmt.Printf("Sum of %d and %d is: %d\n", x, y, x+y)
+	
+}
+
