1f493d40beb3dfef4567632207d8bd66e88b8577
waynetabalanza <tabalanzawayne@gmail.com>
2026-05-22 10:18:00 +0800

    two: store your name and age then print them

---
+++ practice/variables&printing/two/two.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Store your name and age then print them
+
+	var firstname string = "John"
+	var lastname string = "Doe"
+
+	fmt.Printf("Your full name is: %s %s\n", firstname, lastname)
+
+}
