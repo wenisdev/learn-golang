295c009b48b5b997b23dc93aeb0cf4a8309e0b69
waynetabalanza <tabalanzawayne@gmail.com>
2026-05-26 11:39:38 +0800

    Store 5 favorite movies in variables

---
+++ practice/variables&printing/six/six.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	//Store 5 favorite movies in variables
+
+	var movie1 string = "Shawshank Redemption"
+	var movie2 string = "Interstellar"
+	var movie3 string = "The Dark Knight"
+	var movie4 string = "Pulp Fiction"
+	var movie5 string = "Forrest Gump"
+
+	println("My 5 favorite movies are: ", movie1, "\n", movie2, "\n", movie3, "\n", movie4, "\nand", movie5)
+}
