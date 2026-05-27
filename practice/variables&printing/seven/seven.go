8ee978aa5e808d9f4036bb4ac2c4d07c77e6a826
waynetabalanza <tabalanzawayne@gmail.com>
2026-05-27 20:36:03 +0800

    seven: Create variables with different data types

---
+++ practice/variables&printing/seven/seven.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+	// Create variables with different data types
+
+	var name string = "John Doe"
+	var age int = 30
+	var bankBalance float64 = 1000.50
+
+	println("Name:", name)
+	println("Age:", age)
+	println("Bank Balance:", bankBalance)
+
+}
